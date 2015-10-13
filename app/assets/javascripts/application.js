8088f63932ed2fb189bd648a6e5321cfcca8579d
Anthony Ellis <ajellis1989@gmail.com>
2015-10-13 06:16:12 -0400

    Yay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+
+
+ function hideThis() {
+  var number = document.getElementsByClassName("row").length;
+  var location = document.getElementsByClassName("row")[number -1];
+  location.style.display = 'none';
+
+}
+
+
+ function showThis() {
+   var number = document.getElementsByClassName("row").length;
+   var location = document.getElementsByClassName("row")[number -1];
+   location.style.display = 'block';
+ }
+
+function onlyOnce() {
+  var findButton = document.getElementsByClassName('btn').length;
+  document.getElementsByClassName('btn')[findButton-1].disabled = true;
+}
+
+
+
+
+function findStuff() {
+  description  = document.getElementsByName("#anchor-desription").href;
+  students = document.getElementsByName("#anchor-students").href;
+  assignments = document.getElementsByName("#anchor-assignments").href;
+  lessons = document.getElementsByName("#anchor-lessons").href;
+  policies = document.getElementsByName("#anchor-policies").href;
+
+}
